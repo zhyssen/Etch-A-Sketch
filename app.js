c9c51f092c2897c172ddbfb974fa3fe99b2131dd
zhyssen <zinethyssen@gmail.com>
2022-12-02 18:15:06 -0500

    Added ability to change size of box, changed background color

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-let board = document.querySelector(.board)
-board.style.gridTemplateColumns = 'repeat(16 , 1fr)'
-board.style.gridTemplateRows = 'repeat(16 , 1fr)'
+let color = 'black';
+
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let amount = size * size;
+    for(let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener("mouseover", colorSquare)
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement('beforeend', square);
+    }
+};
+populateBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        populateBoard(input);
+    }
+    
+};
+
+function colorSquare() {
+    this.style.backgroundColor = 'color';
+};
+
+function changeColor(choice) {
+    color = choice;
+}
